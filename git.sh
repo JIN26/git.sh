7ded4a1c0eebf53cc1e602145dd3bb567fd16807
JIN26 <Angelmantilla26@gmail.com>
2020-07-26 23:39:00 +0000

    muestra de configuracion y añade la direccion

--- git.sh
+++ git.sh
@@ -8,6 +8,6 @@ git config --global alias.s "status";
 git config --global alias.c "commit -m";
 git config --global alias.lo "log --oneline --decorate --all --graph";
 git config --global alias.p "push -u origin master";
-
 git config --global user.email "Angelmantilla26@gmail.com"
 git config --global user.name "JIN26"
+git config -l
