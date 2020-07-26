ad0f626d95dc2d87cb9e2e46e838b77347db7221
JIN26 <Angelmantilla26@gmail.com>
2020-07-26 23:06:53 +0000

    incorporacion de alias

---
+++ git.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+#My configuration git 
+
+git config --global alias.jin "https://github.com/JIN26/";
+git config --global alias.l "log";
+git config --global alias.s "status";
+git config --global alias.c "commit -m";
+git config --global alias.lo "log --oneline --decorate --all --graph";
+git config --global alias.p "push -u origin master";
+
+git config --global user.email "Angelmantilla26@gmail.com"
+git config --global user.name "JIN26"
