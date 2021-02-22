73d79a1ed0a3e4f49f33ee3f65c58b445cc59e98
JIN26 <Angelmantilla26@gmail.com>
2021-02-22 05:57:54 -0500

    Update vnc.sh con apache2
    add pack apache2
    algunos cambios referentes al curso

--- src/cmd/vnc.sh
+++ src/cmd/vnc.sh
@@ -1,20 +1,52 @@
 #!/bin/bash
 
+jk="$(ping -c 1 -q www.google.com >&/dev/null; echo $?)" #Coneccion de internet
 local=$(hostname -I)
 setterm -foreground green;
-figlet -cf slant "${local:0:11}:8080" ||cowsay sorry
+figlet -cf slant "${local:0:12}:8080" ||cowsay sorry
 
+#chequeo de packete apache2
+case $(command -v apache2) in
+    "")
+    case $jk in
+        0)
+        ;;
+        *)
+            -e "\n$r[-]$b1 banner:$r No Hay Conexion A Internet.\n$b"
+    esac
+    clear
+    apt-get install -y apache2
+    apt-get install -y apache2-doc
+    cd /etc/apache2/sites-available/
+    echo "
+    <VirtualHost *:80>
+        ServerName Jin
+        ServerAlias www.Jin.com
+        ServerAdmin webmaster@localhost
+        DocumentRoot /home/jin/Downloads/0/git.sh/src/webpack/
+        DirectoryIndex index.php
+    </VirtualHost>">Jin.conf;
+    a2ensite Jin.conf
+    cd /var/www/
+    mkdir Jin
+    cd Jin
+    sudo ln -s /home/jin/Downloads/0/git.sh/src/webpack/index.php index.php
+    echo "127.0.0.1     jin">>/etc/hosts
+    echo "Listen 81">>/etc/apache2/ports.conf
+    service apache2 reload
+    netstat -tuna
+esac
 case "$OSTYPE" in
     cygwin*)
         ;;
     linux-androideabi)
         vncserver-stop
         vncserver-start
-        php -S "${local:0:11}:8080" -t ../webpack/php/
+        php -S "${local:0:12}:8080" -t ../webpack/
         ;;
     linux-gnu)
-        google-chrome "${local:0:11}:8080"
-        php -S "${local:0:11}:8080" -t ../webpack/php/
+        google-chrome "${local:0:12}:8080"
+        php -S "${local:0:12}:8080" -t ../webpack/
         ;;
     darwin*)
         ;;
